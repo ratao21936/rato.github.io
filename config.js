e3ae5dd43ef7b1a5ae9360f760b4f36dca8824fe
ratao21936 <mc.ratao21936@gmail.com>
2026-06-06 20:39:10 -0300

    Update video URL in config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ subtitulo:"BRASIL • PROGRAMADOR",
 
 avatar:"https://cdn.discordapp.com/avatars/1231281182948196430/a_24a648641a482c61fc1deff747a9aedc.webp?animated=true&size=2048",
 
-video:"https://SEU_VIDEO.mp4",
+video:"https://cdn.discordapp.com/banners/1231281182948196430/a_23419ffa601f82e4f7475d34a79d9cbb.webp?animated=true&size=512",
 
 sobre:"Construa seu caminho mesmo quando ninguém acreditar.",
 
